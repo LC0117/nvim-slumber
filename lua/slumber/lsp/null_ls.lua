d657d28c07ab8e264fb34b9d0e85776ec37484b0
studierer <77481149+LC0117@users.noreply.github.com>
2022-03-14 00:33:28 +0800

    add some linters

--- lua/slumber/lsp/null_ls.lua
+++ lua/slumber/lsp/null_ls.lua
@@ -1,38 +1,54 @@
 local bf = require('null-ls').builtins.formatting
 local bd = require('null-ls').builtins.diagnostics
 local ba = require('null-ls').builtins.code_actions
+local bc = require('null-ls').builtins.completion
 local methods = require('null-ls').methods
 -- a lot of builtin sources
 local sources_users = {
-  ba.eslint_d,
+  ba.eslint,
+  ba.gitsigns,
   ba.shellcheck,
-  bf.clang_format,
+  bc.tags,
+  bf.black,
+  bf.clang_format.with({
+    filetypes = { 'c', 'cpp' }
+  }),
+  bf.fourmolu,
   bf.cmake_format,
   bf.gofmt,
   bf.google_java_format,
+  bf.ktlint,
+  bf.latexindent,
+  bf.perltidy,
+  bf.pg_format,
+  bf.remark,
+  bf.scalafmt,
   bf.shfmt,
   bf.stylua.with({
     condition = function(utils)
       return utils.root_has_file({ 'stylua.toml', '.stylua.toml' })
     end,
   }),
+  bf.stylelint,
   bf.swiftformat,
   bf.taplo,
   bf.uncrustify.with({
     filetypes = { 'cs', 'csharp' },
   }),
-  bd.php,
   bf.prettierd.with({
     filetypes = { 'markdown' },
   }),
   bd.chktex,
-  bd.codespell.with({
-    extra_args = { '-I', '~/Proj/dict/codespell.txt' },
-  }),
+  bd.checkmake,
+  bd.cspell,
+  bd.php,
   bd.cppcheck,
-  bd.eslint_d,
+  bd.eslint,
   bd.golangci_lint,
+  bd.revive,
+  bd.selene,
   bd.shellcheck,
+  bd.stylelint,
   bd.vint,
 }
 require('null-ls').setup({
