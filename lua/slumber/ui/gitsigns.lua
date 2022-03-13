c2a7d051808f0d725beb4c50f69f5439a44caa38
studierer <77481149+LC0117@users.noreply.github.com>
2022-03-14 01:03:20 +0800

    change some icons and the virtual text's pos

--- lua/slumber/ui/gitsigns.lua
+++ lua/slumber/ui/gitsigns.lua
@@ -1,19 +1,22 @@
 require('gitsigns').setup({
   signs = {
-    add = { hl = 'GitGutterAdd', text = '' },
-    change = { hl = 'GitGutterChange', text = '~' },
+    add = { hl = 'GitGutterAdd', text = '' },
+    change = { hl = 'GitGutterChange', text = '' },
     delete = { hl = 'GitGutterDelete', text = '' },
-    topdelete = { hl = 'GitGutterDeleteChange', text = '~' },
-    changedelete = { hl = 'GitGutterChange', text = '~' },
+    topdelete = { hl = 'GitGutterDeleteChange', text = '' },
+    changedelete = { hl = 'GitGutterChange', text = '' },
   },
   linehl = false,
   numhl = true,
   watch_gitdir = { interval = 1000, follow_files = true },
   current_line_blame = true,
-  current_line_blame_opts = { delay = 500, virtual_text_pos = 'eol' },
+  current_line_blame_opts = { delay = 500, virt_text_pos = 'right_align' },
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
   word_diff = false,
   diff_opts = { internal = true },
+  preview_config = {
+    border = 'rounded'
+  }
 })
