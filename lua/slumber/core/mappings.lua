8c2313eeda569f16f3002fc71887279792d8e2aa
studierer <77481149+LC0117@users.noreply.github.com>
2022-04-28 00:11:53 +0800

    change mappings

--- lua/slumber/core/mappings.lua
+++ lua/slumber/core/mappings.lua
@@ -11,12 +11,15 @@ local function nimap(key, fun)
 end
 
 -- telescope settings
-nmap('<leader>f', [[:Telescope fd<CR>]])
-nmap('<leader>gr', [[:Telescope live_grep<CR>]])
-nmap('<leader>s', require('slumber.plugins.finder.utils').buf_find)
+-- find file
+nmap('<leader>ff', [[:Telescope fd<CR>]])
+-- find word
+nmap('<leader>fw', [[:Telescope live_grep<CR>]])
+-- find current buffer
+nmap('<leader>fb', require('slumber.plugins.finder.utils').buf_find)
 
 -- lsp things
-nmap('<leader>fm', vim.lsp.buf.formatting_sync)
+nmap('<leader>lf', vim.lsp.buf.formatting_sync)
 nmap('<leader>ca', vim.lsp.buf.code_action)
 vmap('<leader>ca', vim.lsp.buf.range_code_action)
 
@@ -27,13 +30,16 @@ nmap('<leader>w', [[:w<CR>]])
 nmap('K', vim.lsp.buf.hover)
 
 -- NvimTree settings
-nmap('<leader>x', [[:Neotree focus<CR>]])
-nmap('<leader>o', [[:Neotree<CR>]])
-nmap('<leader>gs', [[:Neotree git_status<CR>]])
+-- tree focus
+nmap('<leader>tf', [[:Neotree focus<CR>]])
+-- tree toggle
+nmap('<leader>tt', [[:Neotree<CR>]])
+-- tree gitstatus
+nmap('<leader>tg', [[:Neotree git_status<CR>]])
 
 -- gitsigns settings
-nmap(',gb', [[:Gitsigns blame_line<CR>]])
-nmap(',gt', [[:Gitsigns toggle_current_line_blame<CR>]])
+nmap('<leader>gb', [[:Gitsigns blame_line<CR>]])
+nmap('<leader>gt', [[:Gitsigns toggle_current_line_blame<CR>]])
 
 -- bufferline mappings
 nmap('b]', [[:BufferLineCycleNext<CR>]])
