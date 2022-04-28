ea86d0069ab075ca88e9eb10ca898ea444c33cdb
studierer <77481149+LC0117@users.noreply.github.com>
2022-04-28 16:24:46 +0800

    add config for delve

--- lua/slumber/plugins/dap/init.lua
+++ lua/slumber/plugins/dap/init.lua
@@ -1,7 +1,9 @@
-local U = require("slumber.core.utils")
+local U = require('slumber.core.utils')
 local path = U.path
-local dapers = path.concat({vim.fn.stdpath('data'), 'dapers'})
+local dapers = path.concat({ vim.fn.stdpath('data'), 'dapers' })
 local dap = require('dap')
+
+-- define the breakpoints
 vim.fn.sign_define('DapBreakpoint', { text = [[]], texthl = 'DiagnosticWarning' })
 vim.fn.sign_define('DapBreakPointCondition', { text = [[]], texthl = 'DiagnosticInfo' })
 vim.fn.sign_define('DapBreakpointRejected', { text = [[]], texthl = 'DiagnosticError' })
@@ -23,6 +25,36 @@ dap.adapters.python = {
   command = vim.env.HOME .. '/.local/opt/virtualenvs/debugpyenv/bin/python',
   args = { '-m', 'debugpy.adapter' },
 }
+dap.adapters.dlv = function(callback, config)
+  local stdout = vim.loop.new_pipe(false)
+  local handle
+  local pid_or_err
+  local port = 38697
+  local opts = {
+    stdio = { nil, stdout },
+    args = { 'dap', '-l', '127.0.0.1:' .. port },
+    detached = true,
+  }
+  handle, pid_or_err = vim.loop.spawn('dlv', opts, function(code)
+    stdout:close()
+    handle:close()
+    if code ~= 0 then
+      print('dlv exited with code', code)
+    end
+  end)
+  assert(handle, 'Error running dlv: ' .. tostring(pid_or_err))
+  stdout:read_start(function(err, chunk)
+    assert(not err, err)
+    if chunk then
+      vim.schedule(function()
+        require('dap.repl').append(chunk)
+      end)
+    end
+  end)
+  vim.defer_fn(function()
+    callback({ type = 'server', host = '127.0.0.1', port = port })
+  end, 100)
+end
 
 dap.configurations.javascript = {
   {
@@ -66,7 +98,7 @@ dap.configurations.python = {
     type = 'python',
     request = 'launch',
     program = '${file}',
-    pythonPath = function ()
+    pythonPath = function()
       local cwd = vim.fn.getcwd()
       if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
         return cwd .. '/venv/bin/python'
@@ -75,6 +107,29 @@ dap.configurations.python = {
       else
         return '/opt/homebrew/bin/python3'
       end
-    end
-  }
+    end,
+  },
+}
+
+dap.configurations.go = {
+  {
+    type = 'dlv',
+    name = 'Debug',
+    request = 'launch',
+    program = '${file}',
+  },
+  {
+    type = 'dlv',
+    name = 'Debug test',
+    request = 'launch',
+    mode = 'test',
+    program = '${file}',
+  },
+  {
+    type = 'dlv',
+    name = 'Debug test (go.mod)',
+    request = 'launch',
+    mode = 'test',
+    program = './${relativeFileDirname}',
+  },
 }
