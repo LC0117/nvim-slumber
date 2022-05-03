8fe76b4c631d10984f160406ba45e8ebe579a3f6
studierer <77481149+LC0117@users.noreply.github.com>
2022-05-04 00:17:08 +0800

    restore

--- lua/slumber/lsp/init.lua
+++ lua/slumber/lsp/init.lua
@@ -1,14 +1,8 @@
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
   border = 'rounded',
-  relative = 'win',
-  width = 40,
-  height = 15,
 })
 vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
   border = 'rounded',
-  relative = 'win',
-  width = 40,
-  height = 15,
 })
 
 local severity = vim.diagnostic.severity
