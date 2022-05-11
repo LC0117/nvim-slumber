9fab76d353238982ce230881ddd93d6281eea58f
studierer <77481149+LC0117@users.noreply.github.com>
2022-05-11 14:21:09 +0800

    remove cmp-emoji

--- lua/slumber/core/packers/tools.lua
+++ lua/slumber/core/packers/tools.lua
@@ -64,7 +64,6 @@ M['hrsh7th/nvim-cmp'] = {
     { 'saadparwaiz1/cmp_luasnip', after = 'nvim-cmp' },
     { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' },
     { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
-    { 'hrsh7th/cmp-emoji', after = 'nvim-cmp' },
     { 'hrsh7th/cmp-cmdline', after = 'nvim-cmp' },
     {
       'tzachar/cmp-tabnine',
