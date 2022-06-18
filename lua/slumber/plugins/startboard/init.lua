422db3a54a31cd978b6562dfae64bb456db88ca7
studierer <77481149+LC0117@users.noreply.github.com>
2022-06-18 11:04:09 +0800

    use dashboard-nvim

--- lua/slumber/plugins/startboard/init.lua
+++ lua/slumber/plugins/startboard/init.lua
@@ -24,98 +24,50 @@ local avatar = {
 }
 
 local neovim = {
+  '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀  ⠀     ',
+  '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀  ⠀     ',
   ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
   ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
   ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
   ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
   ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
   ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+  '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀  ⠀     ',
+  '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀  ⠀     ',
 }
 
-local header = {
-  type = 'text',
-  oldfiles_directory = false,
-  align = 'center',
-  fold_section = false,
-  title = 'Header',
-  margin = 5,
-  content = avatar,
-  highlight = 'Statement',
-  default_color = '',
-  oldfiles_amount = 0,
-}
-
-local body = {
-  type = 'mapping',
-  oldfiles_directory = false,
-  align = 'center',
-  fold_section = false,
-  title = 'Basic Commands',
-  margin = 5,
-  content = {
-    { ' Find File', 'Telescope find_files', '<leader>ff' },
-    { ' Find Word', 'Telescope live_grep', '<leader>lg' },
-    { ' Recent Files', 'Telescope oldfiles', '<leader>of' },
-    { ' File Browser', 'Telescope file_browser', '<leader>fb' },
-    { ' Colorschemes', 'Telescope colorscheme', '<leader>cs' },
-    { ' New File', 'lua require\'startup\'.new_file()', '<leader>nf' },
+local db = require('dashboard')
+db.hide_statusline = true
+db.hide_tabline = true
+db.custom_header = neovim
+db.custom_footer = function()
+  local clock = ' ' .. os.date('%H:%M')
+  local date = ' ' .. os.date('%d-%m-%y')
+  return { clock, date }
+end
+db.custom_center = {
+  {
+    icon = ' ',
+    desc = 'Find Files          ',
+    action = [[Telescope find_files]],
+    shortcut = 'SPC f f',
   },
-  highlight = 'String',
-  default_color = '',
-  oldfiles_amount = 0,
-}
-local date = {
-  type = 'text',
-  content = function()
-    local clock = ' ' .. os.date('%H:%M')
-    local date = ' ' .. os.date('%d-%m-%y')
-    return { clock, date }
-  end,
-  oldfiles_directory = false,
-  align = 'center',
-  fold_section = false,
-  title = '',
-  margin = 5,
-  highlight = 'TSString',
-  default_color = '#FFFFFF',
-  oldfiles_amount = 10,
-}
-
-local settings = {
-  header = header,
-  body = body,
-  date = date,
-  options = {
-    mapping_keys = true,
-    cursor_column = 0.5,
-    empty_lines_between_mappings = true,
-    disable_statuslines = true,
-    paddings = { 2, 1, 1 },
+  {
+    icon = ' ',
+    desc = 'Find Word           ',
+    action = [[Telescope live_grep]],
+    shortcut = 'SPC l g',
   },
-  colors = {
-    background = '#1f2227',
-    folded_section = '#56b6c2',
+  {
+    icon = ' ',
+    desc = 'File Browser        ',
+    action = [[Telescope file_browser]],
+    shortcut = 'SPC f b',
   },
-  mappings = {
-    execute_command = '<CR>',
-    open_file = 'o',
-    open_file_split = '<c-o>',
-    open_section = '<TAB>',
-    open_help = '?',
-  },
-  parts = {
-    'header',
-    'body',
-    'date',
+  {
+    icon = ' ',
+    desc = 'Recent Files        ',
+    action = [[DashboardFindHistory]],
+    shortcut = 'SPC f h',
   },
 }
-
-require('startup').create_mappings({
-  ['<leader>ff'] = '<cmd>Telescope find_files<CR>',
-  ['<leader>lg'] = '<cmd>Telescope live_grep<CR>',
-  ['<leader>of'] = '<cmd>Telescope oldfiles<CR>',
-  ['<leader>fb'] = '<cmd>Telescope file_browser<CR>',
-  ['<leader>cs'] = '<cmd>Telescope colorscheme<CR>',
-  ['<leader>nf'] = '<cmd>Telescope new_file<CR>',
-})
-require('startup').setup(settings)
