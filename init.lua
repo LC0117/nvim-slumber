1e64a1c5572d606a2c2c0dbc7e36712fa822d088
studierer <77481149+LC0117@users.noreply.github.com>
2022-07-08 13:47:08 +0800

    some modifications

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
--- do
---   local ok, _ = pcall(require, 'impatient')
---   if not ok then
---     vim.notify('impatient.nvim not installed', vim.log.levels.WARN)
---   end
--- end
+do
+  local ok, _ = pcall(require, 'impatient')
+  if not ok then
+    vim.notify('impatient.nvim not installed', vim.log.levels.WARN)
+  end
+end
 
 require('slumber')
