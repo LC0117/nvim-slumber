6c7e7fdbafef50e92281bf865d92c81d46495b6d
studierer <77481149+LC0117@users.noreply.github.com>
2022-09-06 11:41:40 +0800

    add LspAttach event autocmds

---
+++ lua/slumber/lsp/events.lua
@@ -0,0 +1,15 @@
+local defaults = require('slumber.lsp.defaults')
+vim.api.nvim_create_augroup('LspEvents', { clear = true })
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = 'LspEvents',
+  callback = function(args)
+    if not (args.data and args.data.client_id) then
+      return
+    end
+
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    require('lsp-inlayhints').on_attach(client, bufnr)
+    defaults.on_attach(client, bufnr)
+  end,
+})
