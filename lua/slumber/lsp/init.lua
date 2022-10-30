1e9633ce6bf883796d81242d9f5c709ab8ca029c
Chao Li <77481149+LC0117@users.noreply.github.com>
2022-10-31 00:49:06 +0800

    minor change

--- lua/slumber/lsp/init.lua
+++ lua/slumber/lsp/init.lua
@@ -1,10 +1,3 @@
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = 'rounded',
-})
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = 'rounded',
-})
-
 local severity = vim.diagnostic.severity
 vim.diagnostic.config({
   virtual_text = {
